9339995f52b4ba0fc2959250a95647f302407fd5
Satyam Sherkar <satyamsherkar007@gmail.com>
2025-12-16 14:20:53 +0530

    added loop for more chances to guess

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,29 @@ fn main() {
     // rand version ^0.8.0 < ^0.9.0 uses `rand::thread_rng()` and `gen_range(start..=end)`(Deprecated).
     let secret_number = rand::rng().random_range(1..=100); // updated methods.
 
-    println!("The Secret number is: {secret_number}");
+    loop {
+        println!("Please input your guess.");
 
-    println!("Please input your guess.");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    let guess: u32 = guess.trim().parse().expect("Please enter a number!");
+        println!("You guessed: {guess}");
 
-    println!("You guessed: {guess}");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!! 😄"),
-        Ordering::Greater => println!("Too big!! 😄"),
-        Ordering::Equal => println!("You guessed it right!! ✅")
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!! 😄"),
+            Ordering::Greater => println!("Too big!! 😄"),
+            Ordering::Equal => {
+                println!("You guessed it right!! ✅");
+                break;
+            }
+        }
     }
 }
