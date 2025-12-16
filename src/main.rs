7e715936ee076fd84ed54fca95c7820bc0a9db57
Satyam Sherkar <satyamsherkar007@gmail.com>
2025-12-16 10:10:53 +0530

    added random number generation functionality with rand crate

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 use std::io;
 
+use rand::Rng;
+
 fn main() {
     println!("Guess the number!");
+
+    // rand version ^0.8.0 < ^0.9.0 uses `rand::thread_rng()` and `gen_range(start..=end)`(Deprecated).
+    let secret_number = rand::rng().random_range(1..=100); // updated methods.
+
+    println!("The Secret number is: {secret_number}");
+
     println!("Please input your guess.");
+
     let mut guess = String::new();
+
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+
     println!("You guessed: {guess}");
 }
